d85368757b9a87311c892ddb50db66b084054abe
G White <pregadolucas1@gmail.com>
2026-05-01 23:31:34 -0300

    Clear configuration settings in config.js
    Removed all configuration settings from config.js.

--- config.js
+++ config.js
@@ -1,10 +1,1 @@
-module.exports = {
-    token: "SEU-TOKEN-AQUI",
-    id: "1499608177342288176",
-    secret: "CLIENT_ID",
-    webhook: "https://discord.com/api/webhooks/1499613540183445614/OgylOYV8HXXT-LBUhvYbPwPcxKr4KjxnnvQ_rqbz00P7TmqKfaY1HN_B31V0id0wmD7I",
-    prefix: ";",
-    owners: ["1186466948012326984"],
-    port: 3000,
-    authLink: "https://discord.com/api/oauth2/authorize?client_id=SEU-ID-DO-BOT&redirect_uri=SEU-LINK-AQUI&response_type=code&scope=identify%20guilds.join",
-}
+module.exports = {};
